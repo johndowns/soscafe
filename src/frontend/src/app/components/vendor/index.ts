5ed79724b902e0ffee22e91fe80efa419ea886ae
Jack Downs <jack@downs.kiwi>
2020-04-15 12:01:42 +1200

    Addition of Vendor-New-Success page for after form completion.

--- src/frontend/src/app/components/vendor/index.ts
+++ src/frontend/src/app/components/vendor/index.ts
@@ -1,5 +1,7 @@
 export * from './vendor-view/vendor-view.component';
 export * from './vendor-detail/vendor-detail.component';
 export * from './vendor-list/vendor-list.component';
+export * from './vendor-new/vendor-new.component';
+export * from './vendor-new/vendor-new-success/vendor-new-success.component';
 export * from './vendor-payments/vendor-payments.component';
 export * from './vendor-vouchers/vendor-vouchers.component';
