189e505e14d87bfcbd18536df8f396189d9629c9
John Downs <john.downs@microsoft.com>
2020-04-19 15:46:19 +1200

    Case comparison

--- src/api/SosCafe.Admin/UserManagement.cs
+++ src/api/SosCafe.Admin/UserManagement.cs
@@ -67,7 +67,7 @@ namespace SosCafe.Admin
 
         internal static bool GetIsAdminClaim(ClaimsPrincipal claimsPrincipal)
         {
-            var isAdminClaim = (claimsPrincipal.Identity as ClaimsIdentity).Claims.FirstOrDefault(c => c.Type == "extension_IsAdmin")?.Value;
+            var isAdminClaim = (claimsPrincipal.Identity as ClaimsIdentity).Claims.FirstOrDefault(c => c.Type.Equals("extension_IsAdmin", System.StringComparison.InvariantCultureIgnoreCase))?.Value;
             return !string.IsNullOrEmpty(isAdminClaim);
         }
 
